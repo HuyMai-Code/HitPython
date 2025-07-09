2c834f8253dd60ae9888f0449d3758ad91fd5efd
mai quang huy <maiquanghuy206@gmail.com>
2025-07-09 19:26:16 +0700

    hello world

---
+++ HelloWorld.py
@@ -0,0 +1,18 @@
+import time
+
+hi = "Chao mung ban den voi tro choi cuop bong"
+
+for x in hi:
+    print(x, end = '', flush = True)
+    time.sleep(0.1)
+
+#Luat choi
+
+print()
+luat = "\nVD: Co 8 qua bong.\nMoi luot co the lay 1 hoac 2 qua bong.\nAi lay duoc qua bong cuoi cung se la nguoi thắng"
+
+for x in luat:
+    print(x, end = '', flush = True)
+    time.sleep(0.05)
+
+
